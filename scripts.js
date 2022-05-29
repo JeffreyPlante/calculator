e45bf266282ffb333f4f14d56ef61aa5f742fe89
user <jeffdplante@aol.com>
2022-05-29 11:27:10 -0700

    Allow number buttons to update display

--- scripts.js
+++ scripts.js
@@ -1,19 +1,28 @@
 const display = document.querySelector('.output')
 	  clearBtn = document.querySelector('.func-buttons').children[0],
 	  deleteBtn = document.querySelector('.func-buttons').children[1],
-	  numsOpsBtns = document.querySelectorAll('.nums-ops .number'),
+	  numsBtns = document.querySelectorAll('.nums-ops .number'),
 	  divideBtn = document.querySelector('.button.divide'),
 	  multiplyBtn = document.querySelector('.button.multiply'),
 	  subtractBtn = document.querySelector('.button.subtract'),
 	  decimalBtn = document.querySelector('.button.decimal'),
 	  equalsBtn = document.querySelector('.button.equals'),
 	  addBtn = document.querySelector('.button.add');
+	
+numsBtns.forEach((button) =>{
+		button.addEventListener('click', () => {
+			value = button.textContent.toString();
 
-numsOpsBtns.forEach((button) =>{
-		button.addEventListener('click', () => console.log(button.textContent))
+			if (display.textContent === "0"){
+				display.textContent = value;
+			} else {
+				display.textContent += value;
+			}
+		});
 
 });
-const updateDisplay = (num) => display.textContent = num.toString();
+
+// const updateDisplay = (num) => display.textContent = num.toString();
 
 function add(x,y){
 	return x + y;
