a55ce822ec513c51408fc44b9bd78f08878b7afd
user <jeffdplante@aol.com>
2022-05-29 08:32:20 -0700

    Grab all buttons in javascript with querySelector()

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
+const display = document.querySelector('.output')
+	  clearBtn = document.querySelector('.func-buttons').children[0],
+	  deleteBtn = document.querySelector('.func-buttons').children[1],
+	  numsOpsBtns = document.querySelectorAll('.nums-ops .number'),
+	  divideBtn = document.querySelector('.button.divide'),
+	  multiplyBtn = document.querySelector('.button.multiply'),
+	  subtractBtn = document.querySelector('.button.subtract'),
+	  decimalBtn = document.querySelector('.button.decimal'),
+	  equalsBtn = document.querySelector('.button.equals'),
+	  addBtn = document.querySelector('.button.add');
+
+numsOpsBtns.forEach((button) =>{
+		button.addEventListener('click', () => console.log(button.textContent))
+
+});
+const updateDisplay = (num) => display.textContent = num.toString();
+
 function add(x,y){
 	return x + y;
 }
